e64c35d5c77eedb648ce4386dd5f4ae83ddfb001
yamsmdj <yamine.mdj@gmail.com>
2023-12-20 23:45:26 +0100

    regex completed

--- JS/validation.js
+++ JS/validation.js
@@ -3,12 +3,16 @@ const UserRegex = /^[a-zA-Z][a-zA-Z]{3,23}$/;
 const PasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@&#$%]).{8,23}$/;
 const EmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const PhoneNumberRegex = /^(?:\d{1,3})?\d{10,14}$/;
+const SujetRegex = /^[^{}<>$]{10,50}$/;
+const CommentRegex = /^[^{}<>$]{30,500}$/;
 
 // ------- VARIABLE -----------
-let inputEmail= document.querySelector("input[type=email]");
-let inputTel= document.querySelector("input[type=tel]");
-let inputName = document.querySelector(".inputName")
-let inputFname = document.querySelector(".inputFname")
+let inputName = document.querySelector("#input1")
+let inputFname = document.querySelector("#input2")
+let inputTel= document.querySelector("#input3");
+let inputEmail= document.querySelector("#input4");
+let inputSujet = document.querySelector('#input5')
+let inputComment = document.querySelector('#Textarea1')
 const form = document.querySelector('form');
 
 // ------ Validateurs ------
@@ -30,12 +34,12 @@ function addclass(element,regex , value, valid) {
         element.classList.remove('is-valid')
         element.classList.add('is-invalid')
     }}
-    inputName.addEventListener('input', (e)=>{
-        addclass(inputName, UserRegex, e.target.value, nomValid)
-    });
+    inputName.addEventListener('input', (e)=>{addclass(inputName, UserRegex, e.target.value, nomValid)});
     inputFname.addEventListener('input', (e)=>{addclass(inputFname, UserRegex, e.target.value)});
-    inputEmail.addEventListener('input', (e)=>{addclass(inputEmail, EmailRegex, e.target.value)});
     inputTel.addEventListener('input', (e)=>{addclass(inputTel, PhoneNumberRegex, e.target.value)});
+    inputEmail.addEventListener('input', (e)=>{addclass(inputEmail, EmailRegex, e.target.value)});
+    inputSujet.addEventListener('input', (e)=>{addclass(inputSujet, SujetRegex, e.target.value)});
+    inputComment.addEventListener('input', (e)=>{addclass(inputComment, CommentRegex, e.target.value)});
     form.addEventListener('submit', e=> {
         e.preventDefault()
         if (emailValid && nomValid && telValid) {
