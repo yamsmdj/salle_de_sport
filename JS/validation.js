5b67e54a064a8cc119748dbc7377e05730e05d33
yamsmdj <yamine.mdj@gmail.com>
2023-12-21 11:42:08 +0100

    mail send add

--- JS/validation.js
+++ JS/validation.js
@@ -5,7 +5,6 @@ const EmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const PhoneNumberRegex = /^(?:\d{1,3})?\d{10,14}$/;
 const SujetRegex = /^[^{}<>$]{10,50}$/;
 const CommentRegex = /^[^{}<>$]{30,500}$/;
-
 // ------- VARIABLE -----------
 let inputName = document.querySelector("#input1")
 let inputFname = document.querySelector("#input2")
@@ -14,16 +13,14 @@ let inputEmail= document.querySelector("#input4");
 let inputSujet = document.querySelector('#input5')
 let inputComment = document.querySelector('#Textarea1')
 const form = document.querySelector('form');
-
 // ------ Validateurs ------
-let emailValid = false;
-let telValid = false;
 let nomValid = false;
+let prenomValid = false ;
+let telValid = false;
+let emailValid = false;
+let sujetValid = false;
+let msgValid = false;
 
-
-console.log(inputFname);
-console.log(inputEmail);
-console.log(form);
 
 function addclass(element,regex , value, valid) {
     if (regex.test(value)) {
@@ -40,48 +37,24 @@ function addclass(element,regex , value, valid) {
     inputEmail.addEventListener('input', (e)=>{addclass(inputEmail, EmailRegex, e.target.value)});
     inputSujet.addEventListener('input', (e)=>{addclass(inputSujet, SujetRegex, e.target.value)});
     inputComment.addEventListener('input', (e)=>{addclass(inputComment, CommentRegex, e.target.value)});
+    
     form.addEventListener('submit', e=> {
         e.preventDefault()
-        if (emailValid && nomValid && telValid) {
-            
+        if (emailValid && nomValid && telValid && prenomValid && sujetValid && msgValid) {
+
+            Email.send({
+                SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+                To : '',
+                From : "localhost:5500",
+                Subject : "This is the subject",
+                Body : "And this is the body",
+                Attachments : [
+                {
+                    name : "smtpjs.png",
+                    path : "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png"
+                }]
+            }).then(
+              message => alert(message)
+            );
         }
     })
-    // form.addEventListener('input', (e)=>{
-//     e.preventDefault()
-//     console.log(EmailRegex.test(inputEmail.value));
-//     // NOM
-// if (UserRegex.test(inputName.value)) {
-//         inputName.classList.add('is-valid')
-//         inputName.classList.remove('is-invalid')
-//     }
-// else {
-//         inputName.classList.remove('is-valid')
-//         inputName.classList.add('is-invalid')
-//     }
-// })
-//     // EMAIL
-//     form.addEventListener('submit', (e)=>{
-//         e.preventDefault()
-//     if (EmailRegex.test(inputEmail.value)) {
-//         inputEmail.classList.add('is-valid');
-//         inputEmail.classList.remove('is-invalid');
-//     } else {
-//         inputEmail.classList.add('is-invalid');
-//         inputEmail.classList.remove('is-valid');
-//     }
-//     //  TELEPHONE
-//     if (PhoneNumberRegex.test(inputTel.value)) {
-//             inputTel.classList.add('is-valid')
-//             inputTel.classList.remove('is-invalid')
-//         }
-//     else {
-//             inputTel.classList.remove('is-valid')
-//             inputTel.classList.add('is-invalid')
-//         }
-
-//     })
-    
-    
-// // inputEmail.addEventListener('input', (e)=>{
-// //     e.target.value()
-// // })
