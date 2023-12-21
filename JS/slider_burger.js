9908871c54b131e6e4d364bcc81a89729fedb299
Rodrigue Thery <xunder.games69@gmail.com>
2023-12-21 18:39:01 +0100

    modif

--- JS/slider_burger.js
+++ JS/slider_burger.js
@@ -1,18 +1,43 @@
-let navbar = document.querySelector('.navbar');
-let btn = document.querySelector('button')
-let burger = document.querySelector('navbar-toggler-icon')
-let itext = document.getElementById('itext');
-let ihtml = document.querySelector('#ihtml');
+// let navbar = document.querySelector('.navbar');
+// let btn = document.querySelector('button')
+// let burger = document.querySelector('navbar-toggler-icon')
+// let itext = document.getElementById('itext');
+// let ihtml = document.querySelector('#ihtml');
 
-itext.innerText = "le code hexadecimale pour la croix est &#x2715;"
-ihtml.innerHTML = "le code hexadecimale pour la croix est &#x2715;"
+// itext.innerText = "le code hexadecimale pour la croix est &#x2715;"
+// ihtml.innerHTML = "le code hexadecimale pour la croix est &#x2715;"
 
-btn.addEventListener('click', ()=>{
-    navbar.classList.toggle('active');
-    if (navbar.classList.contains('active')) {
-      btn.innerHTML = "&#x2715;";
-    } else {
-      btn.innerHTML = "&#x2630;";
-    }
-  })
-  let slider = document.querySelector('')
+// btn.addEventListener('click', ()=>{
+//     navbar.classList.toggle('active');
+//     if (navbar.classList.contains('active')) {
+//       btn.innerHTML = "&#x2715;";
+//     } else {
+//       btn.innerHTML = "&#x2630;";
+//     }
+//   })
+//   let slider = document.querySelector('')
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
